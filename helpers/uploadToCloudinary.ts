d2f47b3793254a6144a507d1130936a5993b089b
Sakib008 <mdshakib1902@gmail.com>
2025-09-09 03:00:51 +0530

    feat : upload function cloudinary

---
+++ helpers/uploadToCloudinary.ts
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from "cloudinary";
+import streamifier from "streamifier"
+import type { ApiResponse } from "../Types/apiResponse.js";
+
+
+
+const uploadToCloudinary = (buffer: Buffer): Promise<ApiResponse> => {
+    return new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+            { folder: 'store-images' },
+            (error, result) => {
+                if (error) return reject(error);
+                resolve({success: true,message: "Upload Stream on cloudinary", data: result } as ApiResponse);
+            }
+        );
+        streamifier.createReadStream(buffer).pipe(uploadStream);
+    });
+};
+
+export { uploadToCloudinary};
+
