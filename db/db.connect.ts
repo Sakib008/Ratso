091bc743fc8e4d719ac310568a2da877fa34ff2f
Sakib008 <mdshakib1902@gmail.com>
2025-09-09 02:59:23 +0530

    feat : database controller

---
+++ db/db.connect.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+interface DBConnect {
+    connectDB: () => Promise<void>;
+}
+
+const connectDB : DBConnect['connectDB'] = async () => {
+    try {
+        await prisma.$connect();
+        console.log('Database connected');
+    } catch (error) {
+        console.error('Database connection error:', error);
+        process.exit(1);
+    }
+}
+export { connectDB };
