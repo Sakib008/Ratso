6f6a380dcb359af845c953227a456d89df633b7b
Sakib008 <mdshakib1902@gmail.com>
2025-09-09 02:58:06 +0530

    feat : signup or create user controller

---
+++ controllers/auth/signup.controller.ts
@@ -0,0 +1,50 @@
+import prisma from "../../prismaClient.js";
+import bcrypt from "bcryptjs";
+import type { Request, Response } from "express";
+import { sendEmailVerification } from "../../helpers/sendEmailVerification.js";
+
+
+const createUser = async (req:Request, res:Response) => {
+    const { name,address,role,profileImage,isEmailVerified,verificationToken,verificationTokenExpiry,email, password } = req.body;
+    try {
+        let profileImageUrl = profileImage || `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}&background=random&size=128`;
+        if (!email || !password || !name || !address) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+        const existingUser = await prisma.user.findUnique({ where: { email } });
+        const verifyCode = Math.floor(100000 + Math.random()*900000).toString()
+        if(existingUser) {
+        if (existingUser.isEmailVerified) {
+            return res.status(400).json({ message: "User already exists with this email" });
+        }else{
+            const hashedPassword = await bcrypt.hash(password, 10);
+            existingUser.password = hashedPassword
+            existingUser.verificationTokenExpiry = new Date(Date.now() + 3600000)
+            existingUser.verificationToken = verifyCode
+            await prisma.user.update({
+                where: { email },
+                data: existingUser
+            });
+        }
+        } else {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const tokenExpiry = new Date(Date.now() + 3600000)
+            
+
+        await prisma.user.create({
+            data: { name, email, password: hashedPassword, address, role, profileImage : profileImageUrl , isEmailVerified: false, verificationToken: verifyCode, verificationTokenExpiry: tokenExpiry }
+        });
+        }
+        const emailResponse = await sendEmailVerification(email,name, verifyCode);
+        console.log("Email sent response:", emailResponse);
+        if (!emailResponse) {
+            return res.status(500).json({ message: "Failed to send verification email" });
+        }
+        res.status(201).json({ message: "User created successfully. Please verify your email." });
+        }catch (error) {
+        console.error("Error during user creation:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export default createUser;
