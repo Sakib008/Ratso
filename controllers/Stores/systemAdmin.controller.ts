576e8f9187b560934246baf78f4dfd8aebc3713a
Sakib008 <mdshakib1902@gmail.com>
2025-09-09 02:58:46 +0530

    feat : get all stores controller

---
+++ controllers/Stores/systemAdmin.controller.ts
@@ -0,0 +1,59 @@
+import prisma from "../../prismaClient.js";
+import jwt from "jsonwebtoken";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
+import { Role } from "../../generated/prisma/index.js";
+import Email from "next-auth/providers/email";
+import { skip } from "node:test";
+dotenv.config();
+
+export const getAllStores = async (req: Request, res: Response) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Authorization header missing or malformed" });
+    }
+    const {page, limit} = req.query;
+    if (page && isNaN(Number(page))) {
+        return res.status(400).json({ message: "Invalid page number" });
+    }
+    if (limit && isNaN(Number(limit))) {
+        return res.status(400).json({ message: "Invalid limit number" });
+    }
+    const token = authHeader.split(" ")[1] as string;
+    const jwtSecret = process.env.JWT_SECRET as string;
+    if(!jwtSecret) {
+        return res.status(500).json({ message: "JWT secret not configured" });
+    }
+
+    try {
+        jwt.verify(token, jwtSecret);
+        const decoded  = jwt.verify(token, jwtSecret) as { userId: string };
+        const userId = decoded.userId;
+        const authorisedUser = await prisma.user.findUnique({
+            where : { id: userId },
+            select: { role: true }
+        });
+        if (!authorisedUser || authorisedUser.role !== Role.ADMIN) {
+            return res.status(403).json({ message: "Forbidden: Admins only" });
+        }
+        const totalStores = await prisma.store.count();
+      const stores = await prisma.store.findMany({
+        skip: (Number(page) || 1) - 1,
+        take: Number(limit) || 10,
+       select : {
+           id: true,
+           name : true,
+           address : true,
+           email : true,
+           rating : true
+       }
+      })
+        res.status(200).json({ page: Number(page) || 1, limit: Number(limit) || totalStores, total : totalStores, stores });
+    } catch (error) {
+        console.error("Error fetching stores:", error);
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({ message: "Invalid token" });
+        }
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
