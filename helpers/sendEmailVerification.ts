c26173257a6f6690c94abdfa06001c5df4fa1eaa
Sakib008 <mdshakib1902@gmail.com>
2025-09-09 02:59:40 +0530

    feat : email verification function

---
+++ helpers/sendEmailVerification.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Resend } from "resend";
+import VerificationEmail from "./email/VerificationEmail.jsx";
+import type { ApiResponse } from "../Types/apiResponse.js";
+const resend = new Resend(process.env.RESEND_API_KEY);
+export const sendEmailVerification = async (email:string,name: string, verifyCode: string):Promise<ApiResponse> =>{
+    try {
+        await resend.emails.send({
+  from: 'Ratso <onboarding@resend.dev>',
+  to: email,
+  subject: 'Verification Code | Ratso',
+  react: VerificationEmail({ name, otp: verifyCode }),
+});
+        return {
+            success: true,message : "Verification Email Send Successfully "
+        }
+    } catch (error) {
+        console.error("Error sending email:", error);
+        return {
+            success: false,message : "Error in Sending Verification Email"
+        }
+    }
+};
