a745185b5cbd394756b58007d259db8d0ba147f4
Sakib008 <mdshakib1902@gmail.com>
2025-09-09 19:44:06 +0530

    feat : auth route for user

---
+++ routes/auth.route.ts
@@ -0,0 +1,21 @@
+import {
+  createUserRequest,
+  verifyToken,
+  loginUser,
+  changePassword,
+  resetPasswordRequest,
+  resetPassword,
+} from "../controllers/index.js";
+import { authVerify } from "../middleware/auth.middleware.js";
+import express from "express";
+const router = express.Router();
+
+router.post("/signup", createUserRequest);
+router.post("/verify-token", verifyToken);
+router.post("/login", loginUser);
+
+router.post("/change-password", authVerify, changePassword);
+router.post("/reset-password", resetPasswordRequest);
+router.post("/reset-password/:token", resetPassword);
+
+export default router;
