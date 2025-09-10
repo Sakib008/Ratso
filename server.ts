c9fa4d990de31282b41ce11af071d0a40b6e9c88
Sakib008 <mdshakib1902@gmail.com>
2025-09-10 19:12:15 +0530

    fix : updated route and cookieParser in server

--- server.ts
+++ server.ts
@@ -1,12 +1,26 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 import { connectDB } from './db/db.connect.js';
+import authRoute from './routes/auth.route.js'
+import userRoute from './routes/user.route.js'
+import storeRoute from './routes/store.route.js'
+import reviewRoute from './routes/review.route.js'
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 connectDB();
-const cors = require('cors');
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/store', storeRoute);
+app.use('/api/v1/review', reviewRoute);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('API is running...');
@@ -14,5 +28,5 @@ app.get('/', (req: Request, res: Response) => {
 
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
+  console.log(`Server is running on http://localhost:${process.env.PORT || 3004}`);
 });
