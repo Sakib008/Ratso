f2ca7b510026a69445377b750e6e9ad2c09b3428
Sakib008 <mdshakib1902@gmail.com>
2025-09-14 20:47:31 +0530

    refactor : removed unused code and typeError resolved

--- controllers/Stores/systemAdmin.controller.ts
+++ controllers/Stores/systemAdmin.controller.ts
@@ -18,29 +18,15 @@ export const getAllStores = async (req: Request, res: Response) => {
     if (isNaN(pageNumber) || isNaN(limitNumber)) {
         return res.status(400).json({ message: "Invalid page or limit number" });
     }
-    const {id} = req.user as User;
 
     try {
-        const authorisedUser : User = await prisma.user.findUnique({
-            where : { id },
-            select: { role: true }
-        });
-        if (!authorisedUser || authorisedUser.role !== Role.ADMIN) {
-            return res.status(403).json({ message: "Forbidden: Admins only" });
-        }
         const totalStores : number = await prisma.store.count();
-      const stores : Store[] = await prisma.store.findMany({
-        skip: (Number(page) || 1) - 1,
-        take: Number(limit) || 10,
-       select : {
-           id: true,
-           name : true,
-           address : true,
-           email : true,
-           rating : true
-       }
+      const stores  = await prisma.store.findMany({
+        skip: (pageNumber - 1) * limitNumber,
+        take: limitNumber || 10,
+      
       })
-        res.status(200).json({ page: Number(page) || 1, limit: Number(limit) || totalStores, total : totalStores, stores });
+        res.status(200).json({ page: pageNumber || 1, limit: limitNumber || totalStores, total : totalStores, stores });
     } catch (error) {
         console.error("Error fetching stores:", error);
         res.status(500).json({ message: "Internal server error" });
