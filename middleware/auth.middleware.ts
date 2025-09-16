0c23540243f4be424946e0bfb45baac1d9e2dc8b
Sakib008 <mdshakib1902@gmail.com>
2025-09-16 20:52:01 +0530

    fix : auth verify middleware

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -3,37 +3,39 @@ import type { Request, Response, NextFunction } from "express";
 import prisma from "../prismaClient.js";
 import type { User } from "../prisma/types.js";
 
-export const authVerify = async (req:Request,res:Response,next:NextFunction) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "Authorization header missing or malformed" });
+export const authVerify = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
+  const jwtSecret = process.env.JWT_SECRET as string;
+  if (!jwtSecret) {
+    return res.status(500).json({ message: "JWT secret not configured" });
+  }
+  try {
+    const decoded = jwt.verify(token, jwtSecret) as { userId: string };
+    if (!decoded.userId) {
+      return res.status(401).json({ message: "Invalid token" });
     }
-    const token = authHeader.split(" ")[1] as string;
-    const jwtSecret = process.env.JWT_SECRET as string;
-    if(!jwtSecret) {
-        return res.status(500).json({ message: "JWT secret not configured" });
-    }
-    try {
-        const decoded = jwt.verify(token, jwtSecret) as { userId: string };
-        if(!decoded.userId) {
-            return res.status(401).json({ message: "Invalid token" });
-        }
-        const user : User = await prisma.user.findUnique({
-            where: {
-                id: Number(decoded.userId),
-            },
-            select : {
-                id : true,
-                name : true,
-                email : true,
-                role : true,
-                profileImage : true
-            }
-        });
-        req.user = user;
-        next();
-    } catch (error) {
-       
-        res.status(500).json({ message: "Internal server error" });
-    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(decoded.userId),
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        profileImage: true,
+      },
+    });
+    req.user = user as User;
+    next();
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
