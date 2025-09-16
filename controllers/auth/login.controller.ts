c7e5d25cf3566979d8aea6145bda9ffbc9b4d1d2
Sakib008 <mdshakib1902@gmail.com>
2025-09-16 20:51:24 +0530

    fix : added token

--- controllers/auth/login.controller.ts
+++ controllers/auth/login.controller.ts
@@ -2,6 +2,7 @@ import prisma from "../../prismaClient.js";
 import bcrypt from "bcryptjs";
 import type { Request, Response } from "express";
 import type { User } from "../../prisma/types.js";
+import { generateToken } from "../../helpers/generateJwtToken.js";
 
 export const loginUser = async (req:Request, res:Response) => {
     const { email, password } = req.body as User;
@@ -21,8 +22,16 @@ export const loginUser = async (req:Request, res:Response) => {
         if (!isEmailVerified) {
             return res.status(400).json({ message: "Please verify your email before logging in" });
         }
+
+        const token = generateToken(user.id);
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 15 * 24 * 60 * 60 * 1000,
+        });
         
-        return res.status(200).json({ message: "Login successful", user: { id: user.id, name: user.name, email: user.email, role: user.role, profileImage: user.profileImage } });
+        return res.status(200).json({ message: "Login successful"});
     } catch (error) {
         console.error("Error during user login:", error);
         res.status(500).json({ message: "Internal server error" });
